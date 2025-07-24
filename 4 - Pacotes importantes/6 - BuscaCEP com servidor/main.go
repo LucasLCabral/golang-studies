bd83b3a06542b18a111af5ddf8e6e21b1141c6f7
Lucas <lucasludovicocabraal@gmail.com>
2025-07-23 23:23:50 -0300

    Implement ViaCEP handler for GET and POST requests with JSON response

--- 4 - Pacotes importantes/6 - BuscaCEP com servidor/main.go
+++ 4 - Pacotes importantes/6 - BuscaCEP com servidor/main.go
@@ -1,6 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Unidade     string `json:"unidade"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Estado      string `json:"estado"`
+	Regiao      string `json:"regiao"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
 
 func main() {
 	http.HandleFunc("/", BuscaCEPHandler)
@@ -8,5 +28,49 @@ func main() {
 }
 
 func BuscaCEPHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(`{"message": "Hello, World!"}`))
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	switch r.Method {
+	case http.MethodGet:
+			cepParam := r.URL.Query().Get("cep")
+			if cepParam == "" {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			cep, err := BuscaCEP(cepParam)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+
+			json.NewEncoder(w).Encode(cep) // Encode the ViaCEP struct as JSON and write to response
+	case http.MethodPost:
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode("Hello Post")
+	}
+	
+}
+
+func BuscaCEP(cep string) (*ViaCEP, error) {
+	resp, err := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var data ViaCEP
+	err = json.Unmarshal(body, &data) // Unmarshal the JSON response into the ViaCEP struct
+	if err != nil {
+		return nil, err
+	}
+	return &data, nil
 }
