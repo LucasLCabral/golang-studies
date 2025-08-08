3dc1a48365894e59157bc565af1ddbe28116a07e
LucasLCabral <lucasludovicocabraal@gmail.com>
2025-08-08 08:49:38 -0300

    Fuzz testing

--- 8-Testing/1/tax_test.go
+++ 8-Testing/1/tax_test.go
@@ -41,7 +41,23 @@ func BenchmarkCalculateTax(b *testing.B) {
 }
 
 func BenchmarkCalculateTax2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		CalculateTax2(500.0)
 	}
+}
+
+func FuzzCalculateTax(f *testing.F) {
+	seed := []float64{-1, -2, -2.5, 500.0, 100.0, 1501.0}
+	for _, amount := range seed {
+		f.Add(amount)
+	}
+	f.Fuzz(func(t *testing.T, amount float64) {
+		result :=  CalculateTax2(amount)
+		if amount <= 0 && result != 0 {
+			t.Errorf("Received %f but expected 0", result)
+		}
+		if amount >= 20000 && result != 20 {
+			t.Errorf("Received %f but expected 20.0", result)
+		}
+	})
 }
