1c46e75c53324ec7a336475aa88938745fcccb6f
LucasLCabral <lucasludovicocabraal@gmail.com>
2025-08-09 12:09:43 -0300

    interfaces

---
+++ 3-Interfaces/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type Pessoa interface {
+	Ativar()
+	Desativar()
+}
+
+type Cliente struct {
+	Nome  string
+	Idade int
+	Ativo bool
+}
+
+func (c *Cliente) Desativar() {
+	c.Ativo = false
+	fmt.Println("Usuario desativado.")
+}
+
+func (c *Cliente) Ativar() {
+	c.Ativo = true
+	fmt.Println("Usuario ativado.")
+}
+
+func Ativacao(pessoa Pessoa) {
+	pessoa.Ativar()
+}
+
+func Desativacao(pessoa Pessoa) {
+	pessoa.Desativar()
+}
+
+func main() {
+	Lucas := Cliente{
+		Nome:  "Lucas",
+		Idade: 19,
+		Ativo: false,
+	}
+
+	Ativacao(&Lucas)
+	Desativacao(&Lucas)
+}
